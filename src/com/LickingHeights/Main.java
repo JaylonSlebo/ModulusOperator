9b6388fbba11f00c0982277357705868eb3248e4
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-03-05 10:50:51 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,9 +3,16 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        static void methodName();
-        
+        testOddness();
+        keepContained();
+
+     public static void testOddness() {
+         int number;
+         Scanner keyboard;
+         keyboard = new Scanner(System.in);
+         System.out.println("Type in any positive number");
+         number = keyboard.nextInt();
+         System.out.println(number + " is odd: " + (number % 2 != 0));
 
 
 
