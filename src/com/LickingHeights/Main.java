d8e96a92b58146b4fea17a68457fde17ea626260
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-03-05 11:04:32 -0500

    lastDigitMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,13 +1,31 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
+
+
+
 public class Main {
 
     public static void main(String[] args) {
         testOddness();
         keepContained();
+        lastdigit();
+
+
+
+
+
+
+
+    }
+
+
 
      public static void testOddness() {
+
          int number;
+
          Scanner keyboard;
          keyboard = new Scanner(System.in);
          System.out.println("Type in any positive number");
@@ -15,6 +33,7 @@ public class Main {
          System.out.println(number + " is odd: " + (number % 2 != 0));
 
 
+     }
 
             public static void keepContained(){
 
@@ -35,5 +54,40 @@ public class Main {
 
 
 
-    }
-}
+
+
+
+        }
+
+        public static void lastdigit () {
+
+        int number;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Type in a 4 digit number");
+        number= keyboard.nextInt();
+        System.out.println("The last digit of "+number + "is "+ number%10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }}
+
